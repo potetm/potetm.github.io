1b671e0e17edf6506b6d86726d8bffc6b77c4b8d
Tim Pote <tim.pote@cicayda.com>
2014-01-12 21:10:37 -0600

    Got the first pass of my code working.

--- _code/src/blog/responsive_design/core.cljs
+++ _code/src/blog/responsive_design/core.cljs
@@ -1,32 +1,88 @@
 (ns blog.responsive-design.core
-  (:require [blog.responsive-design.template :as t]
+  (:require [clojure.string :as str]
             [jayq.core :refer [$] :as j]
             [yolk.bacon :as b]
             [yolk.jquery :as bjb]))
 
+(def ENTER 13)
+(def TAB 9)
+(def UP_ARROW 38)
+(def DOWN_ARROW 40)
+(def ESC 27)
+
 (defn ->clj [obs]
   (b/map obs #(js->clj % :keywordize-keys true)))
 
-(defn init-model [$elem highlight unhighlight]
-  (j/a))
+(def KEYS #{ENTER TAB UP_ARROW DOWN_ARROW ESC})
+(def keycode->action
+  {ENTER      :select
+   TAB        :select
+   UP_ARROW   :prev
+   DOWN_ARROW :next
+   ESC        :exit})
 
-(defn init-elem [items]
-  (let [$elem ($ (t/ul))]
-    (doseq [item items]
-      (j/append $elem (t/li item)))
+(defn key-events [$elem]
+  (let [mousein (b/map (bjb/mouseenterE $elem) true)
+        mouseout (b/map (bjb/mouseleaveE $elem) false)
+        has-focus? (bjb/model false)]
+    (-> (b/merge mousein mouseout)
+        (->> (bjb/add-source has-focus?)))
 
-    $elem))
+    (-> ($ js/document)
+        bjb/keydownE
+        (b/filter has-focus?)
+        (b/do-action j/prevent)
+        (b/map #(.-keyCode %))
+        (b/filter (comp not nil? KEYS))
+        (b/map #(keycode->action %)))))
 
-(defn menu [items highlight unhighlight]
-  (let [$elem (init-elem items)
-        next nil
-        prev nil
-        highlight nil]
+(defn init-model [items]
+  (bjb/combine-model
+    {:items       items
+     :highlighted -1
+     :selected    -1}))
 
-    {:$elem $elem
-     :in    {:next      next
-             :prev      prev
-             :highlight highlight}}))
+(defn filter-keywords [obs & keys]
+  (let [keys (set keys)]
+    (b/filter obs #(not (nil? (keys %))))))
 
-(let [menu (menu ["a" "s" "d" "f"] nil nil)]
-  (j/html ($ :div#test) (:$elem menu)))
+(defn init-events! [model events]
+  (let [next (filter-keywords events :next)]
+    (-> (b/map next model)
+        ->clj
+        (b/map
+          (fn [{:keys [items highlighted] :as m}]
+            (let [next (if (or (< highlighted 0)
+                               (= (count items) (inc highlighted)))
+                         0
+                         (inc highlighted))]
+              (clj->js (assoc m :highlighted next)))))
+        (->> (bjb/add-source model)))))
+
+(defn menu [items events render]
+  (let [model (init-model items)]
+    (init-events! model events)
+
+    (-> model
+        b/log
+        ->clj
+        (b/on-value
+          (fn [{:keys [items highlighted selected] :as test}]
+            (render items highlighted selected))))))
+
+(let [$elem ($ :pre#array-highlight-list)]
+  (menu
+    ["so" "it" "goes"]
+    (key-events $elem)
+    (fn [items highlighted selected]
+      (->> (map
+             (fn [i item]
+               (str
+                 (if (= i highlighted) ">" " ")
+                 (if (= i selected) "*" " ")
+                 "  "
+                 item))
+             (iterate inc 0)
+             items)
+           (str/join "\n")
+           (j/text $elem)))))
--- _code/src/blog/responsive_design_orig/core.cljs
+++ _code/src/blog/responsive_design_orig/core.cljs
@@ -69,6 +69,26 @@
      (-> (b/filter in (comp not highlighter-filter))
          (b/merge out)))))
 
+(defn selector [in list]
+  (let [highlighted (b/to-property (b/filter in number?))
+        selected (bjb/model nil)
+        prev-cur (b/sliding-window selected 2 2)
+        out (b/bus)]
+    (-> prev-cur
+        (b/on-value
+          (fn [[prev cur]]
+            (when (number? prev)
+              (-unselect! list prev))
+            (-select! list cur)
+            (b/push out cur))))
+    (-> in
+        (b/filter (partial = :select))
+        (b/map highlighted)
+        (->> (bjb/add-source selected)))
+
+    (-> (b/filter in (partial not= :select))
+        (b/merge out))))
+
 (defn keystream [has-focus?]
   (-> ($ js/document)
       bjb/keydownE
@@ -78,16 +98,16 @@
       (b/filter (comp not nil? KEYS))
       (b/map key->keyword)))
 
-(defn create-example [selector render action]
-  (let [$elem ($ selector)
-        mousein (b/map (bjb/mouseenterE $elem) true)
+(defn create-example [$elem render action]
+  (let [mousein (b/map (bjb/mouseenterE $elem) true)
         mouseout (b/map (bjb/mouseleaveE $elem) false)
         has-focus? (bjb/model false)]
     (-> (b/merge mousein mouseout)
         (->> (bjb/add-source has-focus?)))
-    (render)
+    (when render
+      (render))
     (-> (action (keystream has-focus?))
-        (b/on-value render))))
+        (b/on-value #(when render (render))))))
 
 (defn set-char! [s i c]
   (str (.substring s 0 i) c (.substring s (inc i))))
@@ -105,31 +125,55 @@
   (-unselect! [list n]
     (aset list n (set-char! (aget list n) 1 " "))))
 
-(let [$elem ($ :pre#test-ui)
-      ui (array "   Smalltalk"
-                "   Lisp"
-                "   Prolog"
-                "   ML")
-      render #(j/text $elem (.join ui "\n"))]
-  (create-example :pre#test-ui render #(highlighter % ui)))
+(let [$elem ($ :pre#array-highlight-list)
+      list (array "   Alan Kay"
+                  "   J.C.R. Licklider"
+                  "   John McCarthy")
+      render #(j/text $elem (.join list "\n"))
+      action #(highlighter % list)]
+  (create-example $elem render action))
 
-#_(defn selector [in list data]
-  (let [out (chan)]
-    (go (loop [highlighted ::none selected ::none]
-          (let [e (<! in)]
-            (if (= e :select)
-              (do
-                (when (number? selected)
-                  (-unselect! list selected))
-                (if (number? highlighted)
-                  (do
-                    (-select! list highlighted)
-                    (>! out [:select (nth data highlighted)]))
-                  (>! out [:select highlighted]))
-                (recur highlighted highlighted))
-              (do
-                (>! out e)
-                (if (or (= e ::none) (number? e))
-                  (recur e selected)
-                  (recur highlighted selected)))))))
-    out))
+(let [$elem ($ :pre#array-highlight-select-list)
+      list (array "   Smalltalk"
+                  "   Lisp"
+                  "   Prolog"
+                  "   ML")
+      render #(j/text $elem (.join list "\n"))
+      action #(selector (highlighter % list) list)]
+  (create-example $elem render action))
+
+(defn hoverstream [$ui]
+  (-> $ui
+      bjb/mouseoverE
+      (b/map #($ (.-target %)))
+      (b/filter #(j/is % "li"))
+      (b/map #(.index %))))
+
+(defn hover-events [$ui has-focus]
+  (-> $ui
+      bjb/clickE
+      (b/map :select)
+      (b/merge-all (hoverstream $ui) (keystream has-focus))))
+
+(extend-type js/HTMLUListElement
+  ICounted
+  (-count [list]
+    (.-length ($ :li ($ list))))
+
+  IHighlightable
+  (-highlight! [list n]
+    (j/add-class ($ (str "li:nth-child(" n ")") ($ list)) "highlighted"))
+  (-unhighlight! [list n]
+    (j/remove-class ($ (str "li:nth-child(" n ")") ($ list)) "highlighted"))
+
+  ISelectable
+  (-select! [list n]
+    (j/add-class ($ (str "li:nth-child(" n ")") ($ list)) "selected"))
+  (-unselect! [list n]
+    (j/remove-class ($ (str "li:nth-child(" n ")") ($ list)) "selected")))
+
+(let [$elem ($ :ul#ul-highlight-select-list)
+      list ($ :li $elem)
+      action #(selector (highlighter % list) list)]
+  (js/console.log $elem)
+  (create-example $elem nil action))
