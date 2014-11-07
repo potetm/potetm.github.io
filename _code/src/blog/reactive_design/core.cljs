adbfde8c8986c46012d0f9be9c6cff8f16aadb52
Tim Pote <tim.pote@cicayda.com>
2014-11-07 17:15:48 -0600

    Got a little bit of state.

--- _code/src/blog/reactive_design/core.cljs
+++ _code/src/blog/reactive_design/core.cljs
@@ -9,17 +9,52 @@
 
 (enable-console-print!)
 
+(def conn (d/create-conn))
+
+(defn bind
+  ([conn q]
+   (bind conn q (atom nil)))
+  ([conn q state]
+   (let [k (uuid/make-random)]
+     (reset! state (d/q q @conn))
+     (d/listen!
+       conn k
+       (fn [tx-report]
+         (let [novelty (d/q q (:tx-data tx-report))]
+           (when (not-empty novelty) ;; Only update if query results actually changed
+             (reset! state (d/q q (:db-after tx-report)))))))
+     (set! (.-__key state) k)
+     state)))
+
+(defn incr [db attr]
+  (let [[e v] (first
+                (d/q '[:find ?e ?v
+                       :in $ ?a
+                       :where
+                       [_ ?a ?v]]
+                     db
+                     attr))]
+    [[:db/add e attr (inc (or v 0))]]))
+
 (defsubscriber test 5 [:button-clicked]
   (go-loop []
     (when-some [v (<! test)]
-      (println v)
+      (d/transact! conn [[:db.fn/call incr :count]])
       (recur))))
 
+(def query
+  '[:find ?v
+    :in $
+    :where
+    [_ :count ?v]])
+
 (defn hello-reagent []
-  [:div "Hello, Reagent!"
-   [:button {:on-click #(event/publish!
-                         (event/event :button-clicked {} {}))}]])
+  (let [state (bind conn query (atom [[0]]))]
+    [:div (str "Hello, Reagent! " (ffirst @state))
+     [:button {:on-click #(event/publish!
+                           (event/event :button-clicked {} {}))}]]))
 
 (reagent/render-component
-  hello-reagent
+  (fn []
+    [hello-reagent])
   (js/document.getElementById "test-01"))
