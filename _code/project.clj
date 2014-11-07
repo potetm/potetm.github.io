d027b4dda813bee7eed8179643624e150408cff5
Tim Pote <tim.pote@cicayda.com>
2014-11-07 12:45:22 -0600

    Up lib versions.

--- _code/project.clj
+++ _code/project.clj
@@ -3,13 +3,13 @@
   :url "http://potetm.github.io"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.match "0.2.0-rc5"]
-                 [prismatic/dommy "0.1.2"]
+                 [prismatic/dommy "0.1.3"]
                  [jayq "2.5.2"]
                  [yolk "0.10.0"]]
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds
                [{:id           "frp-dev"
                  :source-paths ["src/blog/frp"]
@@ -41,12 +41,14 @@
                  :source-paths ["src/blog/responsive_design_frp"]
                  :compiler     {:optimizations :whitespace
                                 :pretty-print  true
+                                :warnings      false ;; dommy complains, but it's fine
                                 :output-to     "../js/responsive-design-frp.js"}}
                 {:id           "responsive-design-frp"
                  :source-paths ["src/blog/responsive_design_frp"]
                  :compiler     {:optimizations :advanced
                                 :pretty-print  false
                                 :output-to     "../js/responsive-design-frp.js"
+                                :warnings      false ;; dommy complains, but it's fine
                                 :externs       ["externs/bacon.js"
                                                 "externs/bacon-jquery.js"
                                                 "externs/jquery-1.8.js"]}}]})
